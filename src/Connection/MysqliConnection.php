ebd5447fb00c09e6b4544a3efb77d861f1d7f235
Ilija Studen <ilija.studen@activecollab.com>
2016-01-03 15:45:39 +0100

    Remove zeros from formatted query execution time

--- src/Connection/MysqliConnection.php
+++ src/Connection/MysqliConnection.php
@@ -445,7 +445,7 @@ class MysqliConnection implements ConnectionInterface
 
             $result = $this->link->query($prepared_sql);
 
-            $execution_time = number_format(microtime(true) - $microtime, 5, '.', '');
+            $execution_time = rtrim(number_format(microtime(true) - $microtime, 5, '.', ''), '0');
 
             if ($this->log) {
                 if ($result === false) {
