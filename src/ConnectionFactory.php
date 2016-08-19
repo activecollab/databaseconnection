affb0bf68411a2aae05df05904d647e9f2335ddc
Ilija Studen <ilija.studen@activecollab.com>
2016-08-19 11:26:52 +0200

    Extract port from mysqli host

--- src/ConnectionFactory.php
+++ src/ConnectionFactory.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Active Collab DatabaseConnection.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace ActiveCollab\DatabaseConnection;
 
 use ActiveCollab\DatabaseConnection\Connection\MysqliConnection;
@@ -30,7 +39,17 @@ class ConnectionFactory
      */
     public function mysqli($host, $user, $pass, $select_database = '', $set_connection_encoding = null, $set_connection_encoding_with_query = false)
     {
-        $link = $this->mysqliConnectFromParams($host, $user, $pass);
+        if (strpos($host, ':') !== false) {
+            $host_bits = explode(':', $host);
+
+            if (empty($host_bits[1])) {
+                $host_bits[1] = 3306;
+            }
+
+            $link = $this->mysqliConnectFromParams($host_bits[0], (int) $host_bits[1], $user, $pass);
+        } else {
+            $link = $this->mysqliConnectFromParams($host, 3306, $user, $pass);
+        }
 
         if ($set_connection_encoding && !$set_connection_encoding_with_query) {
             $link->set_charset($set_connection_encoding);
@@ -50,16 +69,17 @@ class ConnectionFactory
     }
 
     /**
-     * @param  string     $host
-     * @param  string     $user
-     * @param  string     $pass
-     * @param  string     $select_database
+     * @param  string              $host
+     * @param  int                 $port
+     * @param  string              $user
+     * @param  string              $pass
+     * @param  string              $select_database
      * @return MysqliLink
      * @throws ConnectionException
      */
-    private function mysqliConnectFromParams($host, $user, $pass, $select_database = '')
+    private function mysqliConnectFromParams($host, $port, $user, $pass, $select_database = '')
     {
-        $link = new MysqliLink($host, $user, $pass);
+        $link = new MysqliLink($host, $user, $pass, '', $port);
 
         if ($link->connect_error) {
             throw new ConnectionException('Failed to connect to database. MySQL said: ' . $link->connect_error);
--- test/src/ConnectionFactoryTest.php
+++ test/src/ConnectionFactoryTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Active Collab DatabaseConnection.
+ *
+ * (c) A51 doo <info@activecollab.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace ActiveCollab\DatabaseConnection\Test;
 
 use ActiveCollab\DatabaseConnection\Connection\MysqliConnection;
@@ -11,11 +20,22 @@ use ActiveCollab\DatabaseConnection\ConnectionFactory;
 class ConnectionFactoryTest extends TestCase
 {
     /**
-     * Test mysqli connection
+     * Test mysqli connection.
      */
     public function testMysqli()
     {
         $connection = (new ConnectionFactory())->mysqli('localhost', 'root', '', 'activecollab_database_connection_test');
+
+        $this->assertInstanceOf(MysqliConnection::class, $connection);
+        $connection->disconnect();
+    }
+
+    /**
+     * Test MySQLi connection with port added to hostname.
+     */
+    public function testMysqliWithPortInHostname()
+    {
+        $connection = (new ConnectionFactory())->mysqli('localhost:3306', 'root', '', 'activecollab_database_connection_test');
 
         $this->assertInstanceOf(MysqliConnection::class, $connection);
         $connection->disconnect();
